c76e1a614be83072138eebc3d5619c2cd59ac8c0
imartov <alexandr.kosyrew@mail.ru>
2023-11-08 20:24:23 +0300

    final

--- main.py
+++ main.py
@@ -55,11 +55,39 @@ class ScrapyPage:
         else:
             for i in range(1, total_height, step):
                 driver.execute_script("window.scrollTo(0, {});".format(i))
-        
+
+    def login_recaptha(self, driver) -> None:
+        solver = RecaptchaSolver(driver=driver)
+        wait = WebDriverWait(driver, 10)
+        # enter login
+        wait_random_time(fromm=1.61, to=3.02)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-input-0"]')))
+        self.enter_text_data(driver=driver, xpath='//*[@id="mat-input-0"]', text=os.getenv("LOGIN"))
+
+        # enter password
+        wait_random_time(fromm=1.23, to=3.34)
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-input-1"]')))
+        self.enter_text_data(driver=driver, xpath='//*[@id="mat-input-1"]', text=os.getenv("PASSWORD"))
+
+        # solve captha
+        wait_random_time(fromm=1.36, to=2.36)
+        recaptcha_iframe = wait.until(EC.element_to_be_clickable((By.XPATH, '//iframe[@title="reCAPTCHA"]')))
+        wait_random_time(fromm=3.89, to=5.12)
+        solver.click_recaptcha_v2(iframe=recaptcha_iframe)
+
+        # click button login
+        wait_random_time(fromm=2.01, to=3.34)
+        button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/div/app-login/section/div/div/mat-card/form/button')))
+        button_login.click()
+
+        # click button find entry
+        wait_random_time(fromm=4.0, to=6.12)
+        button_find_entry = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/div/app-dashboard/section[1]/div/div[2]/div/button')))
+        button_find_entry.click()
 
     def find_entry(self):
         ''' main func for scrapy page of visa '''
-        print("Start: ", get_current_time())
+        print("Start: ", get_current_time())     
         try:
             ua = UserAgent()
             random_ua = ua.random
@@ -79,42 +107,15 @@ class ScrapyPage:
             driver = uc.Chrome(browser_executable_path=os.getenv("PATH_BROWSER"),
                                 driver_executable_path=os.getenv("PATH_DRIVER"),
                                 options=options)
-            driver.maximize_window()
-            solver = RecaptchaSolver(driver=driver)
-            wait = WebDriverWait(driver, 10)
             
+            driver.maximize_window()
+            wait = WebDriverWait(driver, 10)
             driver.get(os.getenv("URL"))
 
             # accept cookies
             wait_random_time(fromm=9.25, to=12.6)
             button_accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
             button_accept_cookies.click()
-
-            # enter login
-            wait_random_time(fromm=1.61, to=3.02)
-            wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-input-0"]')))
-            self.enter_text_data(driver=driver, xpath='//*[@id="mat-input-0"]', text=os.getenv("LOGIN"))
-
-            # enter password
-            wait_random_time(fromm=1.23, to=3.34)
-            wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-input-1"]')))
-            self.enter_text_data(driver=driver, xpath='//*[@id="mat-input-1"]', text=os.getenv("PASSWORD"))
-
-            # solve captha
-            wait_random_time(fromm=1.36, to=2.36)
-            recaptcha_iframe = wait.until(EC.element_to_be_clickable((By.XPATH, '//iframe[@title="reCAPTCHA"]')))
-            wait_random_time(fromm=3.89, to=5.12)
-            solver.click_recaptcha_v2(iframe=recaptcha_iframe)
-
-            # click button login
-            wait_random_time(fromm=2.01, to=3.34)
-            button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/div/app-login/section/div/div/mat-card/form/button')))
-            button_login.click()
-
-            # click button find entry
-            wait_random_time(fromm=4.0, to=6.12)
-            button_find_entry = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div/div/app-dashboard/section[1]/div/div[2]/div/button')))
-            button_find_entry.click()
             
             with open("xpaths_visa_select_category.json", "r", encoding="utf-8") as file:
                     xpaths_visa_select_category_dict = json.load(file)
@@ -122,81 +123,86 @@ class ScrapyPage:
             with open("xpaths_visa_type.json", "r", encoding="utf-8") as file:
                 xpaths_visa_type_dict = json.load(file)
             
-            for city, visa_data in xpaths_visa_type_dict.items():
+            while not self.exception:
+                self.login_recaptha(driver=driver)
+                for city, visa_data in xpaths_visa_type_dict.items():
 
-                # click drop-down list select field visa center
-                wait_random_time(fromm=2.6, to=4.5)
-                drop_down_list_field_visa_center = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["center_select_field"])))
-                drop_down_list_field_visa_center.click()
+                    # click drop-down list select field visa center
+                    wait_random_time(fromm=2.6, to=4.5)
+                    drop_down_list_field_visa_center = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["center_select_field"])))
+                    drop_down_list_field_visa_center.click()
 
-                # select visa center
-                wait_random_time(fromm=0.8, to=1.5)
-                select_visa_center = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["center_text"])))
-                actions = ActionChains(driver)
-                actions.move_to_element(select_visa_center).perform()
-                select_visa_center.click()
-
-                # click drop-down list select field visa category
-                wait_random_time(fromm=4.0, to=6.5)
-                drop_down_list_visa_category = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["category_select_field"])))
-                drop_down_list_visa_category.click()
-
-                # select visa catogory
-                wait_random_time(fromm=0.8, to=1.5)
-                select_visa_category = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["category_text"])))
-                select_visa_category.click()
-                
-                if "subcategory" in visa_data:
-                    wait_random_time(fromm=1.0, to=1.5)
-                    self.smooth_scroll(driver=driver, height=0, step=randint(4, 8), end=True)
-                    # click drop-down list select field visa subcategory
-                    wait_random_time(fromm=2.0, to=3.5)
-                    drop_down_list_visa_subcategory = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["subcategory_select_field"])))
-                    drop_down_list_visa_subcategory.click()
-
-                    # select visa subcategory
+                    # select visa center
                     wait_random_time(fromm=0.8, to=1.5)
-                    select_visa_subcategory = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["subcategory_text"])))
-                    select_visa_subcategory.click()
-                else:
+                    select_visa_center = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["center_text"])))
+                    actions = ActionChains(driver)
+                    actions.move_to_element(select_visa_center).perform()
+                    select_visa_center.click()
+
+                    # click drop-down list select field visa category
+                    wait_random_time(fromm=4.0, to=6.5)
+                    drop_down_list_visa_category = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["category_select_field"])))
+                    drop_down_list_visa_category.click()
+
+                    # select visa catogory
+                    wait_random_time(fromm=0.8, to=1.5)
+                    select_visa_category = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["category_text"])))
+                    select_visa_category.click()
+                    
+                    if "subcategory" in visa_data:
+                        wait_random_time(fromm=1.0, to=1.5)
+                        self.smooth_scroll(driver=driver, height=0, step=randint(4, 8), end=True)
+                        # click drop-down list select field visa subcategory
+                        wait_random_time(fromm=2.0, to=3.5)
+                        drop_down_list_visa_subcategory = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["subcategory_select_field"])))
+                        drop_down_list_visa_subcategory.click()
+
+                        # select visa subcategory
+                        wait_random_time(fromm=0.8, to=1.5)
+                        select_visa_subcategory = wait.until(EC.element_to_be_clickable((By.XPATH, visa_data["subcategory_text"])))
+                        select_visa_subcategory.click()
+                    else:
+                        wait_random_time(fromm=1.0, to=1.5)
+                        self.smooth_scroll(driver=driver, height=0, step=randint(4, 8), end=True)
+
+                    # enter birth date
+                    wait_random_time(fromm=3.0, to=5.0)
+                    wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["birth_date_input_field"])))
+                    self.enter_text_data(driver=driver,
+                                            xpath=xpaths_visa_select_category_dict["birth_date_input_field"],
+                                            text=os.getenv("DATE_BIRTH"))
+
+                    # click drop-down list select field citizenship
+                    wait_random_time(fromm=1.0, to=3.0)
+                    drop_down_list_citizenship = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["citizenship_select_field"])))
+                    drop_down_list_citizenship.click()
+
+                    # select citizenship
+                    wait_random_time(fromm=1.5, to=2.5)
+                    select_citizenship = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["citizenship"]["xpath_text"])))
+                    actions = ActionChains(driver)
+                    actions.move_to_element(select_citizenship).perform()
+                    select_citizenship.click()
                     wait_random_time(fromm=1.0, to=1.5)
-                    self.smooth_scroll(driver=driver, height=0, step=randint(4, 8), end=True)
 
-                # enter birth date
-                wait_random_time(fromm=3.0, to=5.0)
-                wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["birth_date_input_field"])))
-                self.enter_text_data(driver=driver,
-                                        xpath=xpaths_visa_select_category_dict["birth_date_input_field"],
-                                        text=os.getenv("DATE_BIRTH"))
+                    # create screen
+                    driver.save_screenshot(os.getenv("PATH_SCREEN_FOLDER"))
 
-                # click drop-down list select field citizenship
-                wait_random_time(fromm=1.0, to=3.0)
-                drop_down_list_citizenship = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["citizenship_select_field"])))
-                drop_down_list_citizenship.click()
-
-                # select citizenship
-                wait_random_time(fromm=1.5, to=2.5)
-                select_citizenship = wait.until(EC.element_to_be_clickable((By.XPATH, xpaths_visa_select_category_dict["citizenship"]["xpath_text"])))
-                actions = ActionChains(driver)
-                actions.move_to_element(select_citizenship).perform()
-                select_citizenship.click()
-                wait_random_time(fromm=1.0, to=1.5)
-
-                # create screen
-                driver.save_screenshot(os.getenv("PATH_SCREEN_FOLDER"))
-
-                # check if message abot no entry is displaying
-                try:
-                    wait = WebDriverWait(driver, 1)
-                    wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[6]/div")))
-                    # wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()=' Приносим извинения, в настоящий момент нет доступных слотов для записи. Пожалуйста, попробуйте позже ']")))
-                    send_message(city=city, visa_center=visa_data["center_text"][15:-2])
-                except Exception as ex:
-                    send_message(city=city, visa_center=visa_data["center_text"][15:-2], entry=True)
-                    print("DOM find entry exception")
-                    print(ex)
-                self.smooth_scroll(driver=driver, height=randint(30, 60), step=randint(4, 8), up=True)
-                print(f"{city} completed: ", datetime.now())
+                    # check if message abot no entry is displaying
+                    try:
+                        wait = WebDriverWait(driver, 1)
+                        wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/div/app-eligibility-criteria/section/form/mat-card[1]/form/div[6]/div")))
+                        # wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()=' Приносим извинения, в настоящий момент нет доступных слотов для записи. Пожалуйста, попробуйте позже ']")))
+                        # send_message(city=city, visa_center=visa_data["center_text"][15:-2])
+                    except Exception as ex:
+                        send_message(city=city, visa_center=visa_data["center_text"][15:-2], time=get_current_time(), entry=True)
+                        print("DOM find entry exception")
+                        print(ex)
+                    self.smooth_scroll(driver=driver, height=randint(30, 60), step=randint(4, 8), up=True)
+                    print(f"{city} completed: ", datetime.now())
+                wait_random_time(fromm=120.0, to=240.0)
+                driver.refresh()
+                wait_random_time(fromm=1.5, to=3.0)
 
         except Exception as ex:
             send_mess_text(path_to_message=os.getenv("TG_MESS_FAIL_SCRIPT"))
@@ -206,7 +212,6 @@ class ScrapyPage:
             driver.close()
             driver.quit()
 
-        # TODO: shedule run
 
 def main():
     sp = ScrapyPage()
